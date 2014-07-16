676de7e3c333dcc4a83e5fc41d08186db7e5ce44
Sandro Padin <sandropadin@gmail.com>
2014-07-16 11:47:52 -0500

    reorganizing spec

--- spec/github_ssh_keys_for/print_keys_spec.rb
+++ spec/github_ssh_keys_for/print_keys_spec.rb
@@ -4,7 +4,8 @@ module GithubSshKeysFor
   describe PrintKeys do
     it 'prints the items in the array one line a time' do
       keys = ['ssh_key key_1', 'ssh_key key_2']
-      expect { described_class.new(keys).execute }.to output("ssh_key key_1\nssh_key key_2\n").to_stdout
+      expect { described_class.new(keys).execute }
+        .to output("ssh_key key_1\nssh_key key_2\n").to_stdout
     end
   end
 end
